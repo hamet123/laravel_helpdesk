1df1e2c7563e8d608581982f739a7ac006ab6e86
Ayush <ayush_94@live.com>
2024-01-05 00:24:21 +0530

    Profile page added more functionality like edit form for profile and changing password

--- resources/views/userPages/myProfile.blade.php
+++ resources/views/userPages/myProfile.blade.php
@@ -15,7 +15,10 @@
     
 }
 .firsthalfdiv{
-    border-right:1px solid white;
+    border:1px solid rgb(160, 160, 160);
+    border-radius:10px;
+    padding-top:20px;
+    padding-bottom:20px;
     display: flex;
       justify-content: center;
       align-items: center;
@@ -24,8 +27,8 @@
 /* Profile picture background styling  */
 #fileInputWrapper {
       position: relative;
-      width: 200px;
-      height: 200px;
+      width: 150px;
+      height: 150px;
       overflow: hidden;
       border-radius: 50%;
       background: url('https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg') center/cover no-repeat;
@@ -37,6 +40,7 @@
       font-size: 16px;
       font-weight: bold;
       /* background-size: fill; */
+      object-fit: cover;
     }
 
     #fileInput {
@@ -65,6 +69,10 @@
         color:white;
         font-size:40px;
     }
+    .socialIcons:hover{
+        transform: scale(1.2);
+        transition: 0.3s ease;
+    }
     .address, .contact{
         
         font-weight: bold;
@@ -83,7 +91,7 @@
 
 
 @section('usercontent')
-<div class="container parentdiv">
+<div class="container parentdiv mt-5">
     <div class="row">
         <div class="col-md-4 firsthalfdiv">
             <div class="container">
@@ -142,6 +150,7 @@
                                     <i class="socialIcons fa-brands fa-square-instagram"></i>
                                 </div>
                             </div>
+                            
                         </div>
                        </div> 
                 </div>
@@ -156,21 +165,66 @@
         <div class="col-md-8 secondhalfdiv">
             <div class="container">
                 <form action="/edit-profile" method="POST">
-                    <div class="row">
+                    <div class="row my-5">
                         <div class="col-md-6">
                             <div class="mb-3">
                                 <label for="name" class="form-label">Full Name</label>
                                 <input type="text" name="name" class="form-control" id="name">
-                              </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="role" class="form-label">Role</label>
+                                <input type="text" name="role" class="form-control" id="role">
+                            </div>
+                            <div class="mb-3">
+                                <label for="phone" class="form-label">Phone</label>
+                                <input type="text" name="phone" class="form-control" id="phone">
+                            </div>
+                            <input type="submit" value="Save" class="btn btn-success">
                         </div>
                         <div class="col-md-6">
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email address</label>
                                 <input type="email" class="form-control" id="email" name="email">
-                              </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" name="username" class="form-control" id="usernameme">
+                            </div>
+                            <div class="mb-3">
+                                <label for="role" class="form-label">Address</label>
+                                <textarea name="Address" class="form-control" rows="1" id="address"></textarea>
+                            </div>
                         </div>
                     </div>
                 </form>
+
+                    <div class="row">
+                        <div class="row my-1">
+                            <div class="col-md-12 p-3" style="border-radius:10px; border-bottom:1px solid rgb(173, 173, 173);">
+                                <ul class="d-flex p-0 justify-content-around align-items-center" style="list-style:none;">
+                                <li class="text-white">Total Tickets Raised : 23</li>
+                                <li class="text-white">Pending Tickets : 8</li>
+                                <li class="text-white">Closed Tickets : 0</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <form action="/change-password" method="POST">
+                        <h5 class="text-white  mt-5 mb-2">Change Your Password</h5>
+                        <div class="row" style="border-radius:10px; border:1px solid rgb(173, 173, 173); padding:20px;">
+                            <div class="col-md-4">
+                                <input type="password" name="password" class="form-control" id="password" placeholder="Enter New Password">
+                            </div>
+                            <div class="col-md-4">
+                                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" placeholder="Confirm New Password">
+                            </div>
+                            <div class="col-md-4">
+                                <input type="submit" value="Change Password" class="btn btn-success">
+                            </div>
+                        </div>
+                    </form>
+                
             </div>
         </div>
     </div>
